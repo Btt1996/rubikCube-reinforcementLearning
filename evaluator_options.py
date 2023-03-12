1f32b5bf1b5434c5531056f2b9e3b06ae6f8984d
ahmed taboubi <61942133+Btt1996@users.noreply.github.com>
2023-03-12 08:47:26 +0100

    Create evaluator_options.py

---
+++ evaluator_options.py
@@ -0,0 +1,6 @@
+# evaluator_options.py
+
+class EvaluatorOptions:
+    def __init__(self, num_episodes=100, max_steps=50):
+        self.num_episodes = num_episodes
+        self.max_steps = max_steps
